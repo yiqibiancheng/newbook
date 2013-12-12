ec8b9556b6ff5d7a39dd6d2366ff901b8a6fd449
yiqibiancheng <yiqibiancheng@gmail.com>
2013-12-12 01:49:32 -0500

    Styled flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,11 @@ class User < ActiveRecord::Base
 
   validates :last_name, presence: true
 
-  validates :profile_name, presence: true,
+   validates :profile_name, presence: true,
                            uniqueness: true,
                            format: {
-                            with: /a-zA-Z0-9_-/,
-                            messagge: 'Must be formatted correctly.'
+                             with: /^[a-zA-Z0-9_-]+$/,
+                             message: 'Must be formatted correctly.'
                            }
 
   has_many :statuses
