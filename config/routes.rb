309c873483e21f031acf97ccae8b82f4fb07b92e
yiqibiancheng <yiqibiancheng@gmail.com>
2013-12-12 02:30:30 -0500

    Added custom routes for auth and book

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Mybook::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
   resources :statuses
+    get 'book', to: 'statuses#index', as: :book
 
   root to: 'statuses#index'
 
